f1fea6edfe06635a77acef4b5cafc1df38c0c8a6
satwik <mohanty.satwik290@gmail.com>
2025-06-02 07:14:06 +0530

    nested html tags

--- app.js
+++ app.js
@@ -1,4 +1,30 @@
-const heading = React.createElement("h1", { id: "heading" }, "hello world from react");
-console.log(heading);//object
+/*
+    <div id="parent">
+   <div id="child1">
+      <h1>h1 tag</h1>
+      <h2>h1 tag</h2>
+   </div>
+   <div id="child2">
+      <h1>h1 tag</h1>
+      <h2>h1 tag</h2>
+   </div>
+ </div>
+*/
+const parent= React.createElement("div",
+    {id:"parent"},
+    React.createElement("div",
+        {id:"child1"},[
+        React.createElement("hi", {}, " i am h1 tag"),
+        React.createElement("h2", {}, " i am h2 tag")]
+    ),
+    React.createElement("div",
+        {id:"child2"},[
+        React.createElement("hi", {}, " i am h1 tag"),
+        React.createElement("h2", {}, " i am h2 tag")]
+    )
+);
+
+
+console.log(parent);//obj
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
