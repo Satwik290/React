195f40e2fe8e5047088435de3633f747922a4fef
satwik <mohanty.satwik290@gmail.com>
2025-06-03 20:41:13 +0530

    update

--- app.js
+++ app.js
@@ -14,12 +14,12 @@ const parent= React.createElement("div",
     {id:"parent"},
     React.createElement("div",
         {id:"child1"},[
-        React.createElement("hi", {}, " i am h1 tag"),
+        React.createElement("h1", {}, " i am h1 tag"),
         React.createElement("h2", {}, " i am h2 tag")]
     ),
     React.createElement("div",
         {id:"child2"},[
-        React.createElement("hi", {}, " i am h1 tag"),
+        React.createElement("h1", {}, " i am h1 tag"),
         React.createElement("h2", {}, " i am h2 tag")]
     )
 );
