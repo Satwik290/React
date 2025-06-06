32f6b1335980ad046f22c14c7f5b46c2040f1997
satwik <mohanty.satwik290@gmail.com>
2025-06-06 21:31:19 +0530

    react--episode2:parcel and react module

--- app.js
+++ app.js
@@ -1,30 +1,29 @@
-/*
-    <div id="parent">
-   <div id="child1">
-      <h1>h1 tag</h1>
-      <h2>h1 tag</h2>
-   </div>
-   <div id="child2">
-      <h1>h1 tag</h1>
-      <h2>h1 tag</h2>
-   </div>
- </div>
-*/
-const parent= React.createElement("div",
-    {id:"parent"},
-    React.createElement("div",
-        {id:"child1"},[
-        React.createElement("h1", {}, " i am h1 tag"),
-        React.createElement("h2", {}, " i am h2 tag")]
-    ),
-    React.createElement("div",
-        {id:"child2"},[
-        React.createElement("h1", {}, " i am h1 tag"),
-        React.createElement("h2", {}, " i am h2 tag")]
-    )
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const parent = React.createElement(
+    "div",
+    { id: "parent" },
+    [
+        React.createElement(
+            "div",
+            { id: "child1" },
+            [
+                React.createElement("h1", {}, " i am h1 tag"),
+                React.createElement("h2", {}, " i am h2 tag")
+            ]
+        ),
+        React.createElement(
+            "div",
+            { id: "child2" },
+            [
+                React.createElement("h1", {}, " i am h1 tag"),
+                React.createElement("h2", {}, " i am h2 tag")
+            ]
+        )
+    ]
 );
 
-
-console.log(parent);//obj
+console.log(parent); // obj
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
