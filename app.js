1964ab8912cb8ebf9ad279984d512729693192ec
satwik <mohanty.satwik290@gmail.com>
2025-06-09 21:51:24 +0530

    episode3: react , jsx,evrything is js

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//react element
-const Title=() => (<h1 className="head" tabIndex="1">
+
+const title=(
+ <h1 className="head" tabIndex="1">
+     Namaste React using JSX
+     </h1>
+);
+const Title=()=>(
+ <h1 className="head" tabIndex="1">
      Namaste React using JSX
      </h1>
 );
 
 
-//react component
-//class based component - old
-// functional component - new
-
 const HeadingComponent = ()=> (    
 <div id="container">
-     {/* component composition */}
-     <Title />
+     {Title()}
+     <Title></Title>
      <h1 className="heading">Namaste React functional component</h1>
      </div>
-
 );
 
 
