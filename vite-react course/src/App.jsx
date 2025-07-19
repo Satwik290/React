fc92ea8f1b2563b6ad6f8754aff9d65a54a7d24f
satwik <mohanty.satwik290@gmail.com>
2025-07-19 21:35:03 +0530

    useState

--- vite-react course/src/App.jsx
+++ vite-react course/src/App.jsx
@@ -1,5 +1,6 @@
 // Import the useState hook from react
 import { useState } from 'react';
+import { PostComponent } from './post';
 
 // Create a function component named App that will be rendered in the root element
 function App() {
@@ -8,7 +9,7 @@ function App() {
         // Apply inline styles to the div element
         <div style={{ backgroundColor: "#dfe6e9", height: "100vh" }}>
             {/* Call ToggleMessage component here to render it in the App component */}
-            <ToggleMessage />
+            <ToggleMessage /> 
 
             <div
                 style={{
@@ -71,58 +72,6 @@ const ToggleMessage = () => {
     );
 };
 
-// Create a style object to apply styles to the div element in PostComponent
-const style = {
-    width: 250,
-    backgroundColor: "white",
-    borderRadius: 10,
-    borderColor: "gray",
-    borderWidth: 1,
-    padding: 20,
-    margin: 10,
-};
-
-// Create a function component named PostComponent with props to render it in the App component
-function PostComponent({ name, subtitle, time, image, description }) {
-    // return JSX that will be rendered
-    return (
-        // Apply style object to the div element
-        <div style={style}>
-            <div style={{ display: "flex" }}>
-                {/* Display the image, name, subtitle, and time */}
-                <img src={image} style={{ width: 40, height: 40, borderRadius: 40 }} />
-                <div style={{ fontSize: 14, marginLeft: 10 }}>
-                    <b>{name}</b> 
-                    <div>{subtitle}</div>
-
-                    {/* conditionally render time if it is available */}
-                    {/* 
-                    {time && (
-                        <div style={{ display: "flex" }}>
-                            <div>{time}</div>
-                            <img
-                                src="https://imgs.search.brave.com/HR4XL7q_XpiQSqNwAl17jYkdNBrGw-fKdXxJlCr-uls/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTM3/MDc3ODM3Ni9waG90/by9jbG9jay02LW9j/bG9jay5qcGc_cz02/MTJ4NjEyJnc9MCZr/PTIwJmM9VTZKUXI5/UFdPYmVuY2lOMHVj/QkgycXJFeWd5WlY4/VUdDem1UQ1pVRnVE/cz0"
-                                style={{ width: 15, height: 15 }}
-                            />
-                        </div>
-                    )} 
-                    */}
-                    {time !== undefined ? (
-                        <div style={{ display: "flex" }}>
-                            <div>{time}</div>
-                            <img
-                                src="https://imgs.search.brave.com/HR4XL7q_XpiQSqNwAl17jYkdNBrGw-fKdXxJlCr-uls/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTM3/MDc3ODM3Ni9waG90/by9jbG9jay02LW9j/bG9jay5qcGc_cz02/MTJ4NjEyJnc9MCZr/PTIwJmM9VTZKUXI5/UFdPYmVuY2lOMHVj/QkgycXJFeWd5WlY4/VUdDem1UQ1pVRnVE/cz0"
-                                style={{ width: 15, height: 15 }}
-                            />
-                        </div>
-                    ) : null}
-                </div>
-            </div>
-
-            <div style={{ fontSize: 14, marginTop: 5 }}>{description}</div>
-        </div>
-    );
-}
 
 // Export App Component to use it in other components
 export default App;
