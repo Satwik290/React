6f663b40cf5377ac33526837eeb1db7e036dc1cc
satwik <mohanty.satwik290@gmail.com>
2025-07-25 21:48:03 +0530

    added profile in main.jsx

--- vite-react course/src/main.jsx
+++ vite-react course/src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Profile from "./profilecomponent.jsx";
+import ProfileComponent from './profilecomponent.jsx';
 
 createRoot(document.getElementById('root')).render(
 
